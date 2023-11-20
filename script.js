2a27d556ac92ff49d05545b000f5da6468aabfca
Himanshu <himanshuydv7777@gmail.com>
2023-11-20 21:27:17 +0530

    animation added

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ var card = document.querySelector(".card")
 var btn = document.querySelector(".form button");
 var qrimg = document.querySelector(".qr-code img")
 var input = document.querySelector(".form input");
-var qr = document.querySelector(".qr-code")
 
 btn.addEventListener("click", function() {
     var txt = input.value;
     if(!txt) return;   //if no text then return from here
+    btn.innerText = "Generating QR......"
     var response = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${txt}`;
     qrimg.src = response;
-    qr.style.display = "block"
+    
+    qrimg.addEventListener("load", function(){
+        btn.innerText = "Generator QR Code"
+        card.classList.add("active")   //add class 'active' to div-card for css animation
+    })
+})
+
+input.addEventListener("keyup", ()=> {
+    if(!input.value){
+        card.classList.remove("active")   //remove class 'active' of div-card if text is removed
+                                          //for css animation
+    }
 })
